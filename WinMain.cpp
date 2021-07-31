7d09bbb4946e9f06872f320ed3cad9dd41d95b9c
CHINA\gWX1027342 <akatokikwok@outlook.com>
2021-07-31 11:05:38 +0800

    [settings]:设置解决方案的诸多选项:使用多字节集并设置为mtd,方便给出exe,并采用fast浮点计算,c++17标准
    Windows程序
    拆分为2部分,一个是窗口,一个是消息
    注册窗口类并创建实例,它并不是C++类,仅需要调用API

--- WinMain.cpp
+++ WinMain.cpp
@@ -4,8 +4,35 @@ int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
-	int       nCmdShow)
+	int       nCmdShow
+)
 {
+	const auto pClassName = "hw3dbutts";
+	// 注册窗口类,使用WindowsAPI
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+	// 创建窗口实例句柄
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	// 呈现展示此窗口(使用句柄)
+	ShowWindow(hWnd, SW_SHOW);
 	while (true);
 	return 0;
 }
