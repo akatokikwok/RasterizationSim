af7e9dc5bf0a75378493395b324f024544526c60
CHINA\gWX1027342 <akatokikwok@outlook.com>
2021-10-11 19:55:43 +0800

    [feat]:设计APP类,应用持有窗口,窗口构造器生成游戏窗口,APP::Go则是消息循环入口

---
+++ App.cpp
@@ -0,0 +1,31 @@
+﻿#include "App.h"
+
+App::App()
+	: wnd(800, 600, "郭的测试窗口-----Box")// 填好窗口尺寸和窗口本身名字; 这个类的构造器和析构器负责创建和销毁窗口; PS!!!若调用2次构造器则允许创建2个窗口
+{}
+
+int App::Go()
+{
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) { // 持续检测消息
+		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+		DoFrame();
+	}
+
+	// check if GetMessage call itself borked
+	if (gResult == -1) {
+		throw CHWND_LAST_EXCEPT();
+	}
+
+	// wParam here is the value passed to PostQuitMessage
+	return msg.wParam;
+}
+
+void App::DoFrame()
+{
+
+}
