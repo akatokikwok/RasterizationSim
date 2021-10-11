c971f8904e64d1732056d8a0954e074577ea0050
CHINA\gWX1027342 <akatokikwok@outlook.com>
2021-10-11 20:04:41 +0800

    [feat]:引入计时器类

---
+++ GrbTimer.cpp
@@ -0,0 +1,21 @@
+﻿#include "GrbTimer.h"
+
+using namespace std::chrono;
+
+GrbTimer::GrbTimer()
+{
+	last = steady_clock::now();
+}
+
+float GrbTimer::Mark()
+{
+	const auto old = last;
+	last = steady_clock::now();
+	const duration<float> frameTime = last - old;
+	return frameTime.count();
+}
+
+float GrbTimer::Peek() const
+{
+	return duration<float>(steady_clock::now() - last).count();
+}
---
+++ GrbTimer.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <chrono>
+
+class GrbTimer
+{
+public:
+	GrbTimer();
+	float Mark();// 返回上次mark到现在 所经历的一段时长
+	float Peek() const;// 与Mark函数作用一致,只不过它不重设计数器
+private:
+	std::chrono::steady_clock::time_point last;
+};
